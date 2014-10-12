0a83f6a4f3d8246696e07b5148ad74779589203d
Hamcha <zikyky@gmail.com>
2014-10-12 16:14:19 +0200

    Stub api

--- TestBot/Program.cs
+++ TestBot/Program.cs
@@ -2,9 +2,27 @@
 // Licensed under GPLv3
 // Refer to the LICENSE.txt file included.
 
+using jzIRC;
+
 namespace TestBot {
     class Program {
+        static IrcConnection server;
+
+        const string serverAddr = "irc.azzurra.org";
+        const int serverPort = 6667;
+
         static void Main(string[] args) {
+            server = new IrcConnection();
+            server.Connected += (conn) => {
+                conn.Join("#jcslab");
+                conn.Joined += (_, chan, me) => {
+                    if (!me) return;
+                    conn.Message(chan, "Hi all!");
+                    conn.Quit("jzIRC iz da bestu!1");
+                };
+            };
+
+            server.Connect(serverAddr, serverPort, false);
         }
     }
 }
