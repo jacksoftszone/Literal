049fec86bde10820d78c9e7459d0a96b15ea6222
Hamcha <zikyky@gmail.com>
2014-10-12 14:56:02 +0200

    Added LICENSE (GPLv3)

--- TestBot/Program.cs
+++ TestBot/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace TestBot {
+﻿// Copyright 2014 #jacksoftszone
+// Licensed under GPLv3
+// Refer to the LICENSE.txt file included.
+
+namespace TestBot {
     class Program {
         static void Main(string[] args) {
         }
