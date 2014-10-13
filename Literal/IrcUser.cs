55c801a16d150a6d221c34d0d96ede323855a13c
Hamcha <zikyky@gmail.com>
2014-10-13 13:38:09 +0200

    IrcUser quick definition

---
+++ Literal/IrcUser.cs
@@ -0,0 +1,14 @@
+﻿namespace Literal {
+    class IrcUser {
+        public string nickname;
+        public string identd;
+        public string hostname;
+        public string realname;
+
+        public static IrcUser fromOrigin(string origin) {
+            char[] splits = { '!', '@' };
+            string[] parts = origin.Split(splits, 3);
+            return new IrcUser { nickname = parts[0], identd = parts[1], hostname = parts[2], realname = "" };
+        }
+    }
+}
