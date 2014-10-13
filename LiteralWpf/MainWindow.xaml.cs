fc8338a33ed1824459c3fe6c66f1d41e972854d6
Hamcha <zikyky@gmail.com>
2014-10-13 21:22:19 +0200

    Basic WPF template

---
+++ LiteralWpf/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace LiteralWpf
+{
+    /// <summary>
+    /// Main window of the client
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
