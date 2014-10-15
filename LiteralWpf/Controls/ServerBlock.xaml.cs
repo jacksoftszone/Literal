83eefd306071a005871b52231194fa611b32e97b
Hamcha <zikyky@gmail.com>
2014-10-15 21:26:33 +0200

    Starting work on serverblock

---
+++ LiteralWpf/Controls/ServerBlock.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LiteralWpf
+{
+	/// <summary>
+	/// Interaction logic for ServerBlock.xaml
+	/// </summary>
+	public partial class ServerBlock : UserControl
+	{
+		public ServerBlock()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
