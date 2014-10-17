aacf9ab3d6111e227fea76b30d69e69307f8e671
Hamcha <zikyky@gmail.com>
2014-10-17 20:08:24 +0200

    Data bindings and better structure

--- LiteralWpf/Controls/ChannelButton.xaml.cs
+++ LiteralWpf/Controls/ChannelButton.xaml.cs
@@ -2,10 +2,30 @@
 // Licensed under GPLv3
 // Refer to the LICENSE.txt file included.
 
+using System.Windows;
 using System.Windows.Controls;
 
 namespace LiteralWpf.Controls {
     public partial class ChannelButton : UserControl {
+
+        public static DependencyProperty ChanNameProperty =
+        DependencyProperty.Register("Channel name", typeof(string), typeof(ServerBlock));
+
+        public string ChanName {
+            get { return GetValue(ChanNameProperty).ToString(); }
+            set { SetNames(value); SetValue(ChanNameProperty, value); }
+        }
+
+        private void SetNames(string channel) {
+            // No prefix
+            if (char.IsLetterOrDigit(channel[0])) {
+                ChannelPrefix.Content = "";
+                ChannelName.Content = channel;
+            } else {
+                ChannelPrefix.Content = channel.Substring(0, 1);
+                ChannelName.Content = channel.Substring(1);
+            }
+        }
 
         public ChannelButton() {
             this.InitializeComponent();
--- LiteralWpf/Controls/ServerBlock.xaml.cs
+++ LiteralWpf/Controls/ServerBlock.xaml.cs
@@ -2,13 +2,20 @@
 // Licensed under GPLv3
 // Refer to the LICENSE.txt file included.
 
+using System.Windows;
 using System.Windows.Controls;
 
 namespace LiteralWpf.Controls {
 
     public partial class ServerBlock : ItemsControl {
 
-        public new string Name;
+        public static DependencyProperty TitleProperty =
+        DependencyProperty.Register("Title", typeof(string), typeof(ServerBlock));
+
+        public string Title {
+            get { return GetValue(TitleProperty).ToString(); }
+            set { SetValue(TitleProperty, value);  }
+        }
 
         public ServerBlock() {
             this.InitializeComponent();
